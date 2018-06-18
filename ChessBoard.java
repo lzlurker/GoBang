a56d716c1ae10d38396a8e05afd82b5a5d1f1933
lzlurker <31750946+lzlurker@users.noreply.github.com>
2018-06-17 23:53:27 -0400

    Add files via upload
    up

--- ChessBoard.java
+++ ChessBoard.java
@@ -4,33 +4,245 @@ import javax.swing.*;
 
 
 public class ChessBoard extends JPanel implements MouseListener{
+	public static final int MARGIN = 30;
+	public static final int GRID_SPAN = 25;
+	public static final int ROWS = 30;
+	public static final int COLS = 30;
+	
+	Pointe[] chesslist = new Pointe[((ROWS+1)*(COLS+1))];
+	boolean isBlack = true;
+	boolean gameOver = false;
+	
+	int chessCount;
+	int xIndex, yIndex;
+	
+	public ChessBoard(){
+		setBackground(Color.orange);
+		addMouseListener(this);
+		addMouseMotionListener(new MouseMotionListener() {
+			
+			@Override
+			public void mouseMoved(MouseEvent e) {
+				// TODO Auto-generated method stub
+				int x1 = (e.getX()-MARGIN+GRID_SPAN/2)/GRID_SPAN;
+				int y1 = (e.getY()-MARGIN+GRID_SPAN/2)/GRID_SPAN;
+				if(x1<0||x1>ROWS||y1<0||y1>COLS||gameOver||findChess(x1,y1)){
+					setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+					
+				}
+				else{
+					setCursor(new Cursor(Cursor.HAND_CURSOR));
+				}
+				
+				
+			}
+			
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+	}
+	
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		for (int i=0;i<=ROWS;i++){
+			g.drawLine(MARGIN, MARGIN+i*GRID_SPAN, MARGIN+COLS*GRID_SPAN, MARGIN+i*GRID_SPAN);
+		}
+		for (int i=0;i<=COLS;i++){
+			g.drawLine(MARGIN+i*GRID_SPAN, MARGIN, MARGIN+i*GRID_SPAN, MARGIN+ROWS*GRID_SPAN);
+		}
+		for (int i=0;i<chessCount;i++){
+			int xPos = chesslist[i].getX()*GRID_SPAN+MARGIN;
+			int yPos = chesslist[i].getY()*GRID_SPAN+MARGIN;
+			g.setColor(chesslist[i].getColor());
+			g.fillOval(xPos-Pointe.DIAMETER/2, yPos-Pointe.DIAMETER/2, Pointe.DIAMETER, Pointe.DIAMETER);
+			
+			if(i == chessCount-1){
+				// last one with red rectangle
+				g.setColor(Color.red);
+				g.drawRect(xPos-Pointe.DIAMETER/2,yPos-Pointe.DIAMETER/2, Pointe.DIAMETER, Pointe.DIAMETER);
+			}
+		}
+		
+		
+	}
+
+	private boolean findChess(int x1, int y1) {
+		// TODO Auto-generated method stub
+		for (Pointe c:chesslist){
+			if(c!=null&&c.getX() == x1 && c.getY()==y1){
+				return true;
+			}
+		}
+		return false;
+	}
 
 	@Override
-	public void mouseClicked(MouseEvent arg0) {
+	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void mouseEntered(MouseEvent arg0) {
+	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void mouseExited(MouseEvent arg0) {
+	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void mousePressed(MouseEvent arg0) {
+	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
+		if(gameOver){
+			return;
+		}
+		String colorName = isBlack? "Black":"White";
+		xIndex=(e.getX()-MARGIN+GRID_SPAN/2)/GRID_SPAN;
+		yIndex=(e.getY()-MARGIN+GRID_SPAN/2)/GRID_SPAN;
 		
+		if(xIndex<0||xIndex>ROWS||yIndex<0||yIndex>COLS){
+			return;
+		}
+		
+		if(findChess(xIndex, yIndex)){
+			return;
+		}
+		
+		Pointe ch = new Pointe(xIndex, yIndex, isBlack?Color.BLACK:Color.white);
+		chesslist[chessCount++]=ch;
+		repaint();
+		
+		if(isWin()){
+			String msg= String.format("%s win!", colorName);
+			JOptionPane.showMessageDialog(this, msg);
+			gameOver=true;
+		}
+		isBlack=!isBlack;
+	}
+
+	private boolean isWin() {
+		// TODO Auto-generated method stub
+		int continueCount = 1;
+		//Horizontal Judgment
+		for (int x=xIndex-1;x>=0;x--){
+			Color c= isBlack?Color.black:Color.white;
+			if(getChess(x,yIndex,c)!=null){
+				continueCount++;
+			}
+			else
+				break;
+		}
+		for (int x=xIndex+1;x<=ROWS;x++){
+			Color c= isBlack?Color.black:Color.white;
+			if(getChess(x,yIndex,c)!=null){
+				continueCount++;
+			}
+			else
+				break;
+		}
+		if(continueCount>4){
+			return true;
+		}
+		else{
+			continueCount = 1;
+		}
+		//End of Horizontal Judgment
+		//Vertical Judgment
+		for (int y=yIndex-1;y>=0;y--){
+			Color c= isBlack?Color.black:Color.white;
+			if(getChess(xIndex,y,c)!=null){
+				continueCount++;
+			}
+			else
+				break;
+		}
+		for (int y=yIndex+1;y<=ROWS;y++){
+			Color c= isBlack?Color.black:Color.white;
+			if(getChess(xIndex,y,c)!=null){
+				continueCount++;
+			}
+			else
+				break;
+		}
+		if(continueCount>4){
+			return true;
+		}
+		else{
+			continueCount = 1;
+		}
+		//End of Vertical Judgment
+		//45 degree Judgment
+		for (int x=xIndex-1,y=yIndex-1;x>=0&&y>=0;x--,y--){
+			Color c= isBlack?Color.black:Color.white;
+			if(getChess(x,y,c)!=null){
+				continueCount++;
+			}
+			else
+				break;
+		}
+		for (int x=xIndex+1,y=yIndex+1;x<=COLS&&y<=ROWS;x++,y++){
+			Color c= isBlack?Color.black:Color.white;
+			if(getChess(x,y,c)!=null){
+				continueCount++;
+			}
+			else
+				break;
+		}
+		if(continueCount>4){
+			return true;
+		}
+		else{
+			continueCount = 1;
+		}
+		//End of 45 degree Judgment
+		//-45 degree Judgment
+		//45 degree Judgment
+		for (int x=xIndex-1,y=yIndex+1;x>=0&&y<=ROWS;x--,y++){
+			Color c= isBlack?Color.black:Color.white;
+			if(getChess(x,y,c)!=null){
+				continueCount++;
+			}
+			else
+				break;
+		}
+		for (int x=xIndex+1,y=yIndex-1;x<=COLS&&y>=0;x++,y--){
+			Color c= isBlack?Color.black:Color.white;
+			if(getChess(x,y,c)!=null){
+				continueCount++;
+			}
+			else
+				break;
+		}
+		if(continueCount>4){
+			return true;
+		}
+		else{
+			continueCount = 1;
+		}
+		//End of -45 degree Judgment
+		return false;
+	}
+
+	private Pointe getChess(int xIndex, int yIndex, Color color) {
+		// TODO Auto-generated method stub
+		for(Pointe c:chesslist){
+			if(c!=null&&c.getX()==xIndex&&c.getY()==yIndex&&c.getColor()==color){
+				return c;
+			}
+		}
+		return null;
 	}
 
 	@Override
-	public void mouseReleased(MouseEvent arg0) {
+	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
